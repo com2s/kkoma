00fa92b0ca51f4c623abe157c5d3b8c38fae50b3
김이현 <rihyun1126@naver.com>
2024-03-14 17:53:56 +0900

    :white_check_mark: test: 거래 글 전체 조회 테스트 코드 작성

---
+++ backend/src/test/java/com/ssafy/kkoma/domain/product/service/ProductServiceTest.java
@@ -0,0 +1,54 @@
+package com.ssafy.kkoma.domain.product.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.ssafy.kkoma.domain.product.dto.response.ProductSummary;
+import com.ssafy.kkoma.domain.product.entity.Product;
+import com.ssafy.kkoma.domain.product.entity.ProductImage;
+import com.ssafy.kkoma.domain.product.repository.ProductImageRepository;
+import com.ssafy.kkoma.domain.product.repository.ProductRepository;
+
+@SpringBootTest
+@EnableJpaAuditing
+class ProductServiceTest {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private ProductImageRepository productImageRepository;
+
+	@Autowired
+	private ProductService productService;
+
+	private static final String TITLE = "TITLE";
+	private static final String IMAGE_URL = "IMAGE_URL";
+
+	@Test
+	public void 글_목록_전체_조회하기() throws Exception{
+	    // given
+		List<Product> products = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+	    	products.add(productRepository.save(Product.builder().title(TITLE).build()));
+		}
+
+		for (Product product : products) {
+		 	productImageRepository.save(ProductImage.builder().product(product).filename(IMAGE_URL).build());
+		}
+
+	    // when
+		List<ProductSummary> productSummaries = productService.getProducts();
+
+		// then
+		assertEquals(10, productSummaries.size());
+	}
+
+}
