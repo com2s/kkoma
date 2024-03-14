1bf4733961252c5be126bf6f3ad29e974da5a5b3
김이현 <rihyun1126@gmail.com>
2024-03-14 21:49:34 +0900

    :sparkles: feat: 거래 글 전체 조회 controller 생성

--- backend/src/main/java/com/ssafy/kkoma/api/product/ProductController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/ProductController.java
@@ -1,10 +1,26 @@
 package com.ssafy.kkoma.api.product;
 
+import com.ssafy.kkoma.domain.product.dto.response.ProductSummary;
+import com.ssafy.kkoma.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/products")
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProductSummary>> getProducts(){
+        List<ProductSummary> products = productService.getProducts();
+        return ResponseEntity.ok(products);
+    }
 
 }
