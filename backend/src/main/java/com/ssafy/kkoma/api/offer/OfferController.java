403a6615ed66ace9701e47c2dcc34c406bc17af7
김이현 <rihyun1126@gmail.com>
2024-03-15 16:09:31 +0900

    :sparkles: feat: 인증 토큰에서 Member 정보 가져오도록 변경

--- backend/src/main/java/com/ssafy/kkoma/api/offer/OfferController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/offer/OfferController.java
@@ -3,6 +3,8 @@ package com.ssafy.kkoma.api.offer;
 import com.ssafy.kkoma.domain.offer.dto.request.OfferTimeRequest;
 import com.ssafy.kkoma.domain.offer.service.OfferDetailService;
 import com.ssafy.kkoma.domain.offer.service.OfferService;
+import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfo;
+import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +20,8 @@ public class OfferController {
     private final OfferDetailService offerDetailService;
 
     @PostMapping("/products/{productId}")
-    public ResponseEntity<?> createOffer(@PathVariable Long productId, @RequestBody List<OfferTimeRequest> offerTimeRequestList){
-        Long offerId = offerService.createOffer(1L, productId);
+    public ResponseEntity<?> createOffer(@MemberInfo MemberInfoDto memberInfo, @PathVariable Long productId, @RequestBody List<OfferTimeRequest> offerTimeRequestList){
+        Long offerId = offerService.createOffer(memberInfo.getMemberId(), productId);
 
         offerDetailService.createOfferDetail(offerId, offerTimeRequestList);
 
