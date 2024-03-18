692da89eafdc9fa1dcd6759e21b6f9bf1268490a
siyoon <ryuu.public@gmail.com>
2024-03-18 10:51:55 +0900

    :sparkles: feat: API documentation

---
+++ backend/src/main/java/com/ssafy/kkoma/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.ssafy.kkoma.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("S10P22A308 APIs")
+                .description("This is API specification for S10P22A308")
+                .version("1.0.0");
+    }
+
+}
