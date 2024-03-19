a74d39c0945d1546f1f805f538e886e9f089290f
soyeon <jjellyduck@gmail.com>
2024-03-19 10:53:22 +0900

    :recycle: refactor: edit import code
    #S10P22A308-288

--- backend/src/main/java/com/ssafy/kkoma/domain/offer/entity/Offer.java
+++ backend/src/main/java/com/ssafy/kkoma/domain/offer/entity/Offer.java
@@ -17,10 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
