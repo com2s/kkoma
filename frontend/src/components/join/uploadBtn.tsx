9147e1da27fba5c7d10eba2fd6ba87cef78ff1c9
zoowb <qlenfrl101@naver.com>
2024-03-19 17:19:52 +0900

    💄 feat: add profile preview
    #S10P22A308-262

--- frontend/src/components/join/uploadBtn.tsx
+++ frontend/src/components/join/uploadBtn.tsx
@@ -2,6 +2,7 @@
 
 import { styled } from "@mui/material/styles";
 import styles from "./uploadBtn.module.scss";
+import { useState, useEffect } from "react";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -16,10 +17,44 @@ const VisuallyHiddenInput = styled("input")({
 });
 
 export default function UploadBtn() {
+  const [imgFile, setImgFile] = useState<File | null>();
+  const [preview, setPreview] = useState<string | null>("");
+
+  const onChangeImg = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files !== null) {
+      const file = event.target.files[0];
+      if (file && file.type.substring(0, 5) === "image") {
+        setImgFile(file);
+      } else {
+        setImgFile(null);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (imgFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(imgFile);
+    } else {
+      setPreview(null);
+    }
+  }, [imgFile]);
+
   return (
     <div className={styles.container}>
-      <label className={styles.uploadBtn}>
-        <VisuallyHiddenInput type="file" accept="image/*" />
+      <label
+        className={styles.uploadBtn}
+        style={{
+          backgroundImage: preview ? `url(${preview})` : "",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+        }}
+      >
+        <VisuallyHiddenInput type="file" accept="image/*" onChange={onChangeImg} />
       </label>
     </div>
   );
