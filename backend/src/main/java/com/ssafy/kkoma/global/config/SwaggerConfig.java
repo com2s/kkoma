97fd6120e516dd464af1a9a228d640109c85d056
soyeon <jjellyduck@gmail.com>
2024-03-20 16:25:11 +0900

    :sparkles: feat: add swagger server
    #S10P22A308-290

--- backend/src/main/java/com/ssafy/kkoma/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/kkoma/global/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.ssafy.kkoma.global.config;
 
+import java.util.Arrays;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,16 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+
+        Server testServer = new Server();
+        testServer.setDescription("prod");
+        testServer.setUrl("https://j10a308.p.ssafy.io");
+
         return new OpenAPI()
+                .servers(Arrays.asList(localServer, testServer))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
