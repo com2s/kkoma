a48f0c872cc0bdf4587b4d7691609016d972769f
황인규 <rlawldbs42@gmail.com>
2024-03-22 00:49:56 +0900

    ✨ feat: API 요청 함수 추가
    #S10P22A308-258

--- frontend/src/components/my-trade/my-trade-ftn.tsx
+++ frontend/src/components/my-trade/my-trade-ftn.tsx
@@ -277,3 +277,32 @@ export async function getRequesters() {
   ];
   return requesters;
 }
+
+import APIModule from "@/utils/apiModule";
+// import exp from "constants";
+
+// export const updateMemberAPI = async (memberInfo: MemberInfo) => {
+//   const res = await APIModule({ action: "/members", method: "PUT", data: memberInfo });
+//   const obj = await res.json();
+
+//   return obj;
+// };
+interface TypeData {
+    type: 'sell' | 'buy' | 'progress';
+}
+
+export async function getMyProducts(type: TypeData['type']) {
+  const queryObject = {
+    type: type,
+  };
+
+  const query = new URLSearchParams(queryObject).toString();
+
+  const response = await APIModule({
+    action: `/members/products?${query}`,
+    method: "GET",
+    data: null,
+  });
+
+  return response.json();
+}
