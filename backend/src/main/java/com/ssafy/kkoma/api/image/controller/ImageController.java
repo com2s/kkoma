ee44b07c31160efe2ec7d4febd0275af533f48bf
soyeon <jjellyduck@gmail.com>
2024-03-21 10:27:07 +0900

    :memo: chore: add swagger doc of image upload api
    #S10P22A308-291

--- backend/src/main/java/com/ssafy/kkoma/api/image/controller/ImageController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/image/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.ssafy.kkoma.api.image.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.kkoma.api.image.service.ImageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +26,11 @@ public class ImageController {
 
 	private final ImageService imageService;
 
-	@PostMapping("/upload")
-	ResponseEntity<?> uploadImages(@RequestPart("images") List<MultipartFile> files) {
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Operation(summary = "to upload images", security = { @SecurityRequirement(name = "bearer-key") })
+	ResponseEntity<List<String>> uploadImages(
+			@Parameter(description = "multipart/form-data 형식의 이미지 리스트를 input으로 받습니다. 업로드 할 수 있는 최대 용량은 100MB 입니다.")
+			@RequestPart("images") List<MultipartFile> files) {
 		List<String> urls = imageService.uploadImages(files);
 		return ResponseEntity.ok().body(urls);
 	}
