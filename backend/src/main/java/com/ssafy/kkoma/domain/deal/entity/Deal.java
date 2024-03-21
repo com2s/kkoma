338acbdbbc71bd9f2689d148696eda1f66fe482f
이수현 <olafminion0116@naver.com>
2024-03-21 09:07:55 +0900

    :label: feat: add variables and set FetchType.LAZY to Deal entity
    #S10P22A308-305

--- backend/src/main/java/com/ssafy/kkoma/domain/deal/entity/Deal.java
+++ backend/src/main/java/com/ssafy/kkoma/domain/deal/entity/Deal.java
@@ -1,12 +1,14 @@
 package com.ssafy.kkoma.domain.deal.entity;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import com.ssafy.kkoma.domain.common.entity.BaseTimeEntity;
 import com.ssafy.kkoma.domain.member.entity.Member;
 import com.ssafy.kkoma.domain.product.entity.Product;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,10 +19,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Builder
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Deal extends BaseTimeEntity {
@@ -29,15 +33,21 @@ public class Deal extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id")
 	private Product product;
 
 	private LocalDateTime selectedTime;
+
+	@Builder.Default
+	private Boolean isCompleted = Boolean.FALSE;
+
+	@Builder.Default
+	private String code = String.valueOf(UUID.randomUUID());
 
 	private LocalDateTime cancelledAt;
 
