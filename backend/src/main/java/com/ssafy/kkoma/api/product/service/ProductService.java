d001cbd16c92ad0ee17551ca2c1d68b4d9715f42
김이현 <rihyun1126@naver.com>
2024-03-21 18:10:28 +0900

    :bug: fix: 거래 글 상태 오류 해결

--- backend/src/main/java/com/ssafy/kkoma/api/product/service/ProductService.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/service/ProductService.java
@@ -21,6 +21,7 @@ import com.ssafy.kkoma.domain.product.entity.Category;
 
 import com.ssafy.kkoma.domain.product.entity.ProductImage;
 import com.ssafy.kkoma.global.error.ErrorCode;
+import com.ssafy.kkoma.global.error.exception.BusinessException;
 import com.ssafy.kkoma.global.error.exception.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
