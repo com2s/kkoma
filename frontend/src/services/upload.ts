f03ca19cff9bb0dba1e6cb61552418e8ad1b7bc7
zoowb <qlenfrl101@naver.com>
2024-03-25 13:23:16 +0900

    🎨 fix: change APIModule format
    #S10P22A308-368

--- frontend/src/services/upload.ts
+++ frontend/src/services/upload.ts
@@ -3,14 +3,23 @@ import { getAccessToken } from "@/utils/getAccessToken";
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const uploadImagesAPI = async (images: FormData) => {
-  const accessToken = await getAccessToken();
-  const res = await fetch(`${baseURL}/images/upload`, {
-    method: "POST",
-    headers: {
-      Authorization: accessToken ?? "",
-    },
-    body: images,
-  });
-  const obj = res.json();
-  return obj;
+  try {
+    const accessToken = await getAccessToken();
+    const res = await fetch(`${baseURL}/images/upload`, {
+      method: "POST",
+      headers: {
+        Authorization: accessToken ?? "",
+      },
+      body: images,
+    });
+    const obj = await res.json();
+    if (obj.success) {
+      return obj.data;
+    } else {
+      //TODO: error 페이지로 이동
+      throw new Error(obj.error.errorMessage);
+    }
+  } catch (e: any) {
+    //TODO: error 페이지로 이동
+  }
 };
