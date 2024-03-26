956e8e752bf2651582d7d99c8ca84c35475e6983
zoowb <qlenfrl101@naver.com>
2024-03-26 12:36:57 +0900

    ✨ feat: 거래 일정 목록 및 거래 확정 api 연결
    #S10P22A308-369

--- frontend/src/components/plan/qr-img.tsx
+++ frontend/src/components/plan/qr-img.tsx
@@ -3,14 +3,17 @@
 import { useParams } from "next/navigation";
 import Image from "next/image";
 
-export default function QRCode() {
-  const APPURL = process.env.NEXT_PUBLIC_APP_URL;
+interface Code {
+  code: string;
+}
 
+export default function QRCode({ code }: Code) {
+  const APPURL = process.env.NEXT_PUBLIC_APP_URL;
   const params = useParams<{ id: string }>();
 
   return (
     <Image
-      src={`https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=${APPURL}/plan/${params.id}/mid`}
+      src={`https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=${APPURL}/plan/${params.id}/progress?code=${code}`}
       alt="qrcode"
       width="250"
       height="250"
