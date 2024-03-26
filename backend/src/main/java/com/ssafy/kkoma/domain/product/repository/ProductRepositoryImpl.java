4392f8024e177e1736c080a38987335397d05dcc
soyeon <jjellyduck@gmail.com>
2024-03-26 10:35:38 +0900

    :sparkles: feat: add filter
    #S10P22A308-359

--- backend/src/main/java/com/ssafy/kkoma/domain/product/repository/ProductRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/kkoma/domain/product/repository/ProductRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.kkoma.api.product.dto.request.SearchProductRequest;
+import com.ssafy.kkoma.domain.product.constant.ProductType;
 import com.ssafy.kkoma.domain.product.entity.Product;
 import static com.ssafy.kkoma.domain.product.entity.QProduct.product;
 
@@ -28,15 +29,13 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 		return categoryId == null ? null : product.category.id.eq(categoryId);
 	}
 
-	// TODO: age filter expression
-	// private BooleanExpression ageGoe(Integer ageGoe) {
-	// 	return ageGoe == null ? null : product.
-	// }
-
 	// TODO: location filter expression
 
-	// TODO: status filter expression
+	private BooleanExpression statusEq(ProductType status) {
+		return status == null ? null : product.status.eq(status);
+	}
 
 	// TODO: keyword filter expression
+	// private BooleanExpression
 
 }
