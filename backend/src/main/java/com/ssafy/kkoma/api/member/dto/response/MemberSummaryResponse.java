9c4cfe04c7d0766708840809a5652f0006cff446
김이현 <rihyun1126@naver.com>
2024-03-26 10:33:55 +0900

    :label: feat: 거래 글 상세 조회 시, response에 memberId 추가

--- backend/src/main/java/com/ssafy/kkoma/api/member/dto/response/MemberSummaryResponse.java
+++ backend/src/main/java/com/ssafy/kkoma/api/member/dto/response/MemberSummaryResponse.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 @Builder
 public class MemberSummaryResponse {
 
+    private Long id;
     private String profileImage;
     private String nickname;
     private String preferredPlace;
 
     public static MemberSummaryResponse fromEntity(Member member){
         return MemberSummaryResponse.builder()
+                .id(member.getId())
                 .profileImage(member.getProfileImage())
                 .nickname(member.getNickname())
                 .build();
