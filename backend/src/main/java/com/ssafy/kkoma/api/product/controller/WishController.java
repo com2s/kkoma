7ab8299fdeb6fcc70f23743eb0db7cd12811c1f0
siyoon <ryuu.public@gmail.com>
2024-03-27 17:15:56 +0900

    :memo: docs: APIs about wish list

--- backend/src/main/java/com/ssafy/kkoma/api/product/controller/WishController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/controller/WishController.java
@@ -5,6 +5,9 @@ import com.ssafy.kkoma.api.product.dto.ProductWishResponse;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfo;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfoDto;
 import com.ssafy.kkoma.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,11 @@ public class WishController {
 
     private final ProductService productService;
 
+    @Tag(name = "Product")
+    @Operation(
+            summary = "to add product in wish list",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @PostMapping("/{productId}/wish")
     public ResponseEntity<ApiUtils.ApiResult<ProductWishResponse>> wishProduct(@MemberInfo MemberInfoDto memberInfoDto, @PathVariable Long productId) {
         Long memberId = memberInfoDto.getMemberId();
@@ -23,6 +31,12 @@ public class WishController {
         return ResponseEntity.ok(ApiUtils.success(response));
     }
 
+
+    @Tag(name = "Product")
+    @Operation(
+            summary = "to remove product in wish list",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @PutMapping("/{productId}/unwish")
     public ResponseEntity<?> unwishProduct(@MemberInfo MemberInfoDto memberInfoDto, @PathVariable Long productId) {
         Long memberId = memberInfoDto.getMemberId();
