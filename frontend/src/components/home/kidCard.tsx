4902397ce1aab5c56bd09cdf68d837ccd5b32c16
zoowb <qlenfrl101@naver.com>
2024-03-27 15:43:52 +0900

    ✨ feat: 메인화면 아기 카드 만들기 완료
    #S10P22A308-372

--- frontend/src/components/home/kidCard.tsx
+++ frontend/src/components/home/kidCard.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import styles from "./kidCard.module.scss";
+import { KidBirthFormat } from "@/utils/format";
+import { KidSummary } from "@/types/kid";
 
-export default function BabyCard() {
+export default function KidCard(props: KidSummary) {
   return (
-    <div className={styles.card}>
+    <>
       <div className={styles["btn-container"]}>
         <MoreHorizIcon className={styles.btn} />
       </div>
       <img src="/images/baby-img.png" alt="baby" />
-      <span className="text-deco">예콩이</span>
-      <span className="text-body2 c-text2">10개월 (D+311)</span>
-    </div>
+      <span className="text-deco">{props.name ?? "이름을 알려주세요"}</span>
+      <span className="text-body2 c-text2">
+        {props.birthDate ? KidBirthFormat(props.birthDate) : "생년월일을 알려주세요"}
+      </span>
+    </>
   );
 }
--- frontend/src/components/home/recommandProductList.tsx
+++ frontend/src/components/home/recommandProductList.tsx
@@ -1,10 +1,14 @@
 import ProductCard from "@/components/home/productCard";
 import styles from "./recommandProductList.module.scss";
 
-export default function recommandProductList() {
+interface Recommand {
+  name: string | null;
+}
+
+export default function RecommandProductList({ name }: Recommand) {
   return (
     <div className={styles.container}>
-      <h4>예콩이를 위한 추천 상품</h4>
+      <h4>{name}에게 추천하는 상품</h4>
       <div className={styles.list}>
         <ProductCard />
         <ProductCard />
