f6d1ac66fb0da5ad0708f1cd9573e154fbc5bef2
김이현 <rihyun1126@naver.com>
2024-03-27 16:18:37 +0900

    :wrench: chore: 채팅 관련 config 파일 수정

--- backend/src/main/java/com/ssafy/kkoma/global/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/kkoma/global/config/WebSocketConfig.java
@@ -1,18 +1,26 @@
 package com.ssafy.kkoma.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.ssafy.kkoma.global.interceptor.AuthenticationInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	private final AuthenticationInterceptor authenticationInterceptor;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/chat")
+		registry.addEndpoint("/api/chat")
 			.setAllowedOriginPatterns("*")
 			.withSockJS();
 	}
