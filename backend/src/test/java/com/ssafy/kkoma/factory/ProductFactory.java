de52a6ceeca3dcd62090f5e7272056134379539d
이수현 <olafminion0116@naver.com>
2024-03-28 14:53:46 +0900

    ✅ test: test 편의성 위해 factory 추가
    #S10P22A308-156

--- backend/src/test/java/com/ssafy/kkoma/factory/ProductFactory.java
+++ backend/src/test/java/com/ssafy/kkoma/factory/ProductFactory.java
@@ -1,6 +1,5 @@
 package com.ssafy.kkoma.factory;
 
-import com.ssafy.kkoma.domain.chat.entity.ChatRoom;
 import com.ssafy.kkoma.domain.kid.constant.GenderType;
 import com.ssafy.kkoma.domain.kid.entity.Kid;
 import com.ssafy.kkoma.domain.kid.repository.KidRepository;
@@ -21,42 +20,30 @@ public class ProductFactory {
     @Autowired
     ProductRepository productRepository;
 
-    private static String TITLE = "TITLE";
+    private static String TITLE = "TEST_상품";
     private static String IMAGE_URL = "https://lh3.googleusercontent.com/wAPeTvxh_EwOisF8kMR2L2eOrIOzjfA5AjE28W5asyfGeH85glwrO6zyqL71dCC26R63chADTO7DLOjnqRoXXOAB8t2f4C3QnU6o0BA";
 
     public ProductFactory(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(Member member) {
+    public Product createProduct(Member seller) {
         Product product = Product.builder()
                 .title(TITLE)
                 .thumbnailImage(IMAGE_URL)
                 .build();
-        product.setMember(member);
+        product.setMember(seller);
         return productRepository.save(product);
     }
 
-    public Product createProduct(Member member, Category category, int price) {
+    public Product createProduct(Member seller, Category category, int price) {
         Product product = Product.builder()
                 .title(TITLE)
                 .category(category)
                 .thumbnailImage(IMAGE_URL)
                 .price(price)
                 .build();
-        product.setMember(member);
-        return productRepository.save(product);
-    }
-
-    public Product createProduct(Member member, Category category, ChatRoom chatRoom, int price) {
-        Product product = Product.builder()
-                .title(TITLE)
-                .category(category)
-                .chatRoom(chatRoom)
-                .thumbnailImage(IMAGE_URL)
-                .price(price)
-                .build();
-        product.setMember(member);
+        product.setMember(seller);
         return productRepository.save(product);
     }
 
