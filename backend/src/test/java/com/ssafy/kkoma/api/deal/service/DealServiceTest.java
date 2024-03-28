853a801e7c7899e0c64cbdb055e46181fb649e1b
이수현 <olafminion0116@naver.com>
2024-03-28 16:48:09 +0900

    ✅ test: 거래 예정 거래건 조회 test 임의 시간값 조정

--- backend/src/test/java/com/ssafy/kkoma/api/deal/service/DealServiceTest.java
+++ backend/src/test/java/com/ssafy/kkoma/api/deal/service/DealServiceTest.java
@@ -1,30 +1,10 @@
 package com.ssafy.kkoma.api.deal.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.ssafy.kkoma.factory.CategoryFactory;
-import com.ssafy.kkoma.factory.MemberFactory;
-import com.ssafy.kkoma.factory.OfferFactory;
-import com.ssafy.kkoma.factory.ProductFactory;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.ssafy.kkoma.api.deal.dto.request.DecideOfferRequest;
 import com.ssafy.kkoma.api.member.service.MemberService;
 import com.ssafy.kkoma.api.offer.service.OfferService;
 import com.ssafy.kkoma.domain.deal.entity.Deal;
 import com.ssafy.kkoma.domain.deal.repository.DealRepository;
-import com.ssafy.kkoma.domain.member.constant.MemberType;
-import com.ssafy.kkoma.domain.member.constant.Role;
 import com.ssafy.kkoma.domain.member.entity.Member;
 import com.ssafy.kkoma.domain.member.repository.MemberRepository;
 import com.ssafy.kkoma.domain.offer.entity.Offer;
@@ -33,8 +13,25 @@ import com.ssafy.kkoma.domain.product.entity.Category;
 import com.ssafy.kkoma.domain.product.entity.Product;
 import com.ssafy.kkoma.domain.product.repository.CategoryRepository;
 import com.ssafy.kkoma.domain.product.repository.ProductRepository;
+import com.ssafy.kkoma.factory.CategoryFactory;
+import com.ssafy.kkoma.factory.MemberFactory;
+import com.ssafy.kkoma.factory.OfferFactory;
+import com.ssafy.kkoma.factory.ProductFactory;
 import com.ssafy.kkoma.global.error.ErrorCode;
 import com.ssafy.kkoma.global.error.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @SpringBootTest
@@ -88,7 +85,7 @@ class DealServiceTest {
 		Deal deal = dealService.createDeal(offer, decideOfferRequest);
 
 	    // then
-		assertEquals(deal.getProduct().getId(), product.getId());
+		Assertions.assertThat(deal.getProduct().getId()).isEqualTo(product.getId());
 	}
 
 	@Test
@@ -109,13 +106,13 @@ class DealServiceTest {
 
 		// 1) 구매자가 거래 코드 요청
 		String buyerReqCode = dealService.getCode(deal.getId(), buyer.getId());
-		assertEquals(deal.getCode(), buyerReqCode);
+		Assertions.assertThat(deal.getCode()).isEqualTo(buyerReqCode);
 
 		// 2) 비구매자가 거래 코드 요청
 		BusinessException exception = assertThrows(BusinessException.class, () -> {
 			dealService.getCode(deal.getId(), seller.getId());
 		});
-		assertEquals(exception.getErrorCode(), ErrorCode.INVALID_BUYER);
+		Assertions.assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.INVALID_BUYER);
 	}
 
 	@Test
@@ -134,8 +131,11 @@ class DealServiceTest {
 
 		String codeSellerGotFromBuyer = dealService.getCode(deal.getId(), buyer.getId()); // 판매자가 구매자의 QR코드를 읽어 코드를 얻었다
 
+		// when
+		Deal resultDeal = dealService.finishDeal(deal.getId(), seller.getId(), codeSellerGotFromBuyer);
+
 		// when & then
-		assertNotNull(dealService.finishDeal(deal.getId(), seller.getId(), codeSellerGotFromBuyer));
+		Assertions.assertThat(resultDeal).isNotNull();
 	}
 
 	@Test
@@ -143,24 +143,16 @@ class DealServiceTest {
 	void 거래_예정_거래_조회() throws Exception {
 		// given
 		LocalDateTime now = LocalDateTime.now();
-		Category category = categoryFactory.createCategory("목욕 용품");
-		Member seller = memberFactory.createMember();
 
 		List<LocalDateTime> dealTimeList = List.of(new LocalDateTime[]{
-			now.plusHours(1),
-			now.plusHours(1).plusMinutes(1).withSecond(0).withNano(0),
-			now.plusHours(1).withSecond(59)
+			now.plusHours(1).withSecond(0).withNano(0), // 성공
+			now.plusHours(1).plusMinutes(1).withSecond(0).withNano(100000000), // 실패
+			now.plusHours(1).withSecond(59) // 성공
 		});
 
 		for (LocalDateTime dealTime : dealTimeList) {
-			Product product = productFactory.createProduct(seller, category, 20000);
-			Member buyer = memberFactory.createMember();
-			Offer offer = offerFactory.createOffer(product, buyer);
-			DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder()
-					.selectedTime(dealTime)
-					.build();
-			offerService.acceptOffer(offer.getId(), decideOfferRequest);
-//			log.info("dealTime은 : {}", dealTime);
+			log.info("거래 시간은 {}", dealTime);
+			createDeal(dealTime);
 		}
 
 		Pageable pageable = PageRequest.of(0,10);
@@ -169,7 +161,20 @@ class DealServiceTest {
 		List<Deal> scheduledDealList = dealService.findScheduledDeal(now, pageable);
 
 		// then
-		assertEquals(2, scheduledDealList.size());
+		Assertions.assertThat(scheduledDealList.size()).isEqualTo(2);
+	}
+
+	/*-----------------------------------------------------------------------------------------------------------*/
+
+	private void createDeal(LocalDateTime dealTime) {
+		Member seller = memberFactory.createMember();
+		Member buyer = memberFactory.createMember();
+
+		Category category = categoryFactory.createCategory("목욕 용품");
+		Product product = productFactory.createProduct(seller, category, 20000);
+		Offer offer = offerFactory.createOffer(product, buyer);
+		DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder().selectedTime(dealTime).build();
+		offerService.acceptOffer(offer.getId(), decideOfferRequest);
 	}
 
 }
