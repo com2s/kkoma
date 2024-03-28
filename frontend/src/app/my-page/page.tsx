728089b22e6cfd346d93b8761ae00007b971e509
황인규 <rlawldbs42@gmail.com>
2024-03-28 15:31:56 +0900

    ✨ feat: 요청 캘린더 수정
    #S10P22A308-392

--- frontend/src/app/my-page/page.tsx
+++ frontend/src/app/my-page/page.tsx
@@ -6,29 +6,7 @@ import MyPoints from "@/components/my-page/my-points";
 import MyPageList from "@/components/my-page/my-page-list";
 import Navigation from "@/components/common/navigation";
 
-async function getChildren() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  // 로딩을 확인하기 위해 임의의 시간(ms) 지연
-  return [
-    { id: 1, name: "Child 1" },
-    { id: 2, name: "Child 2" },
-    { id: 3, name: "Child 3" },
-    { id: 4, name: "Child 4" },
-    { id: 5, name: "Child 5" },
-    { id: 6, name: "Child 6" },
-    { id: 7, name: "Child 7" },
-    { id: 8, name: "Child 8" },
-    { id: 9, name: "Child 9" },
-    { id: 10, name: "Child 10" },
-  ];
-  // TODO: 데이터 페칭
-  // const response = await fetch(API_URL);
-  // const json = await response.json();
-  // return json;
-}
-
 export default async function MyPage() {
-  const children = await getChildren();
   return (
     <div className={styles.container}>
       <TopBar />
