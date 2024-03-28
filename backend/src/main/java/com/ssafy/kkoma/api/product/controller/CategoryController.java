c30c21686431f2060ca2ddf583339ea3db343782
soyeon <jjellyduck@gmail.com>
2024-03-28 12:44:41 +0900

    :memo: chore: add category swagger doc
    #S10P22A308-364

--- backend/src/main/java/com/ssafy/kkoma/api/product/controller/CategoryController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/controller/CategoryController.java
@@ -12,6 +12,9 @@ import com.ssafy.kkoma.api.product.service.CategoryService;
 import com.ssafy.kkoma.domain.product.entity.Category;
 import com.ssafy.kkoma.global.util.ApiUtils;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,12 +24,22 @@ public class CategoryController {
 
 	private final CategoryService categoryService;
 
+	@Tag(name = "Category")
+	@Operation(
+		summary = "카테고리 리스트 전체 조회",
+		security = { @SecurityRequirement(name = "bearer-key") }
+	)
 	@GetMapping
 	public ResponseEntity<ApiUtils.ApiResult<List<Category>>> getCategories() {
 		List<Category> response = categoryService.getCategories();
 		return ResponseEntity.ok(ApiUtils.success(response));
 	}
 
+	@Tag(name = "Category")
+	@Operation(
+		summary = "카테고리 id로 조회",
+		security = { @SecurityRequirement(name = "bearer-key") }
+	)
 	@GetMapping("/{categoryId}")
 	public ResponseEntity<ApiUtils.ApiResult<Category>> getCategory(@PathVariable Integer categoryId) {
 		Category response = categoryService.findCategoryById(categoryId);
