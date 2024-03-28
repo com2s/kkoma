720a85453186905df6511ed389882ad6877fcae7
이수현 <olafminion0116@naver.com>
2024-03-28 16:46:32 +0900

    🐛 fix: Member entity cascade 속성 삭제

--- backend/src/main/java/com/ssafy/kkoma/domain/product/entity/Product.java
+++ backend/src/main/java/com/ssafy/kkoma/domain/product/entity/Product.java
@@ -35,7 +35,7 @@ public class Product extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
