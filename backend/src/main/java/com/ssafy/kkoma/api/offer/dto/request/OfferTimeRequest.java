1a6fa8029e647f94ad573f28ab4eb2918430850d
이수현 <olafminion0116@naver.com>
2024-03-28 15:17:56 +0900

    ♻️ chore: dto에 lombok annotation 추가
    #S10P22A308-156

--- backend/src/main/java/com/ssafy/kkoma/api/offer/dto/request/OfferTimeRequest.java
+++ backend/src/main/java/com/ssafy/kkoma/api/offer/dto/request/OfferTimeRequest.java
@@ -1,19 +1,25 @@
 package com.ssafy.kkoma.api.offer.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OfferTimeRequest {
 
     private LocalDate offerDate;
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public OfferTimeRequest(LocalDate offerDate, LocalTime startTime, LocalTime endTime){
+    public OfferTimeRequest(LocalDate offerDate, LocalTime startTime, LocalTime endTime) {
         this.offerDate = offerDate;
         this.startTime = startTime;
         this.endTime = endTime;
--- backend/src/main/java/com/ssafy/kkoma/api/product/dto/ProductCreateRequest.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/dto/ProductCreateRequest.java
@@ -2,13 +2,17 @@ package com.ssafy.kkoma.api.product.dto;
 
 import com.ssafy.kkoma.domain.product.entity.Product;
 import com.ssafy.kkoma.domain.product.entity.ProductImage;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCreateRequest {
 
     private List<String> productImages;
