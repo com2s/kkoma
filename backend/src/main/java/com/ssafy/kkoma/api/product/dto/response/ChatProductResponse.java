9fa8955e2008b25b7ff2ed28cf279c89e9a0ea70
김이현 <rihyun1126@naver.com>
2024-03-28 10:53:27 +0900

    :sparkles: feat: 채팅방에서 거래 글 정보 조회 기능 구현

---
+++ backend/src/main/java/com/ssafy/kkoma/api/product/dto/response/ChatProductResponse.java
@@ -0,0 +1,26 @@
+package com.ssafy.kkoma.api.product.dto.response;
+
+import com.ssafy.kkoma.domain.product.entity.Product;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ChatProductResponse {
+
+	private Long id;
+	private String thumbnailImage;
+	private String title;
+	private int price;
+
+	public static ChatProductResponse fromEntity(Product product) {
+		return ChatProductResponse.builder()
+			.id(product.getId())
+			.thumbnailImage(product.getThumbnailImage())
+			.title(product.getTitle())
+			.price(product.getPrice())
+			.build();
+	}
+
+}
