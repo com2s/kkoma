3dc094564e46b8157160abd6ece84c825669eade
이수현 <olafminion0116@naver.com>
2024-03-28 15:16:00 +0900

    ✅ test: DealServiceTest factory 이용해 정리, findScheduledDeal() test code 추가
    #S10P22A308-156

--- backend/src/test/java/com/ssafy/kkoma/api/deal/service/DealServiceTest.java
+++ backend/src/test/java/com/ssafy/kkoma/api/deal/service/DealServiceTest.java
@@ -2,11 +2,20 @@ package com.ssafy.kkoma.api.deal.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.ssafy.kkoma.factory.CategoryFactory;
+import com.ssafy.kkoma.factory.MemberFactory;
+import com.ssafy.kkoma.factory.OfferFactory;
+import com.ssafy.kkoma.factory.ProductFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.kkoma.api.deal.dto.request.DecideOfferRequest;
@@ -27,8 +36,18 @@ import com.ssafy.kkoma.domain.product.repository.ProductRepository;
 import com.ssafy.kkoma.global.error.ErrorCode;
 import com.ssafy.kkoma.global.error.exception.BusinessException;
 
+@Slf4j
 @SpringBootTest
 class DealServiceTest {
+
+	@Autowired
+	MemberFactory memberFactory;
+	@Autowired
+	CategoryFactory categoryFactory;
+	@Autowired
+	OfferFactory offerFactory;
+	@Autowired
+	ProductFactory productFactory;
 
 	@Autowired
 	CategoryRepository categoryRepository;
@@ -54,21 +73,15 @@ class DealServiceTest {
 	@Autowired
 	DealService dealService;
 
-	private static final String TITLE = "TITLE";
-	private static final String IMAGE_URL = "IMAGE_URL";
-	private static final String NAME = "NAME";
-	private static final String NAME_BUYER = "BUYER";
-	private static final String NAME_SELLER = "SELLER";
-
 	@Test
 	@Transactional
 	public void 거래_생성() throws Exception {
 	    // given
-		Category category = categoryRepository.save(Category.builder().name("유모차").build());
-		Member member = memberRepository.save(Member.builder().name(NAME).memberType(MemberType.KAKAO).role(Role.USER).build());
-		Product product = productRepository.save(Product.builder().title(TITLE).thumbnailImage(IMAGE_URL).category(category).member(member).build());
-
-		Offer offer = offerRepository.save(Offer.builder().product(product).member(member).build());
+		Category category = categoryFactory.createCategory("목욕 용품");
+		Member seller = memberFactory.createMember();
+		Product product = productFactory.createProduct(seller, category, 20000);
+		Member buyer = memberFactory.createMember();
+		Offer offer = offerFactory.createOffer(product, buyer);
 		DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder().selectedTime(LocalDateTime.now()).build();
 
 		// when
@@ -80,43 +93,26 @@ class DealServiceTest {
 
 	@Test
 	@Transactional
-	void 구매자가_거래_코드_요청() throws Exception {
+	void 거래_코드_요청() throws Exception {
 		// given
-		Category category = categoryRepository.save(Category.builder().name("유모차").build());
-		Member seller = memberRepository.save(Member.builder().name(NAME_SELLER).memberType(MemberType.KAKAO).role(Role.USER).build());
-		Member buyer = memberRepository.save(Member.builder().name(NAME_BUYER).memberType(MemberType.KAKAO).role(Role.USER).build());
-		Product product = productRepository.save(Product.builder().title(TITLE).thumbnailImage(IMAGE_URL).category(category).member(seller).build());
-
-		Offer offer = offerRepository.save(Offer.builder().product(product).member(buyer).build());
+		Category category = categoryFactory.createCategory("목욕 용품");
+		Member seller = memberFactory.createMember();
+		Product product = productFactory.createProduct(seller, category, 20000);
+		Member buyer = memberFactory.createMember();
+		Offer offer = offerFactory.createOffer(product, buyer);
 		DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder().selectedTime(LocalDateTime.now()).build();
 
-		// when
 		offerService.acceptOffer(offer.getId(), decideOfferRequest);
 		Deal deal = dealRepository.findByProduct(product);
+
+		// when & then
+
+		// 1) 구매자가 거래 코드 요청
 		String buyerReqCode = dealService.getCode(deal.getId(), buyer.getId());
-
-		// then
 		assertEquals(deal.getCode(), buyerReqCode);
-	}
 
-	@Test
-	@Transactional
-	void 비구매자가_거래_코드_요청() throws Exception {
-		// given
-		Category category = categoryRepository.save(Category.builder().name("유모차").build());
-		Member seller = memberRepository.save(Member.builder().name(NAME_SELLER).memberType(MemberType.KAKAO).role(Role.USER).build());
-		Member buyer = memberRepository.save(Member.builder().name(NAME_BUYER).memberType(MemberType.KAKAO).role(Role.USER).build());
-		Product product = productRepository.save(Product.builder().title(TITLE).thumbnailImage(IMAGE_URL).category(category).member(seller).build());
-
-		Offer offer = offerRepository.save(Offer.builder().product(product).member(buyer).build());
-		DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder().selectedTime(LocalDateTime.now()).build();
-
-		// when
-		offerService.acceptOffer(offer.getId(), decideOfferRequest);
-		Deal deal = dealRepository.findByProduct(product);
-
-		// then
-		BusinessException exception = (BusinessException) assertThrows(BusinessException.class, () -> {
+		// 2) 비구매자가 거래 코드 요청
+		BusinessException exception = assertThrows(BusinessException.class, () -> {
 			dealService.getCode(deal.getId(), seller.getId());
 		});
 		assertEquals(exception.getErrorCode(), ErrorCode.INVALID_BUYER);
@@ -126,26 +122,54 @@ class DealServiceTest {
 	@Transactional
 	void 거래_완료_처리() throws Exception {
 		// given
-		Category category = categoryRepository.save(Category.builder().name("유모차").build());
-		Member seller = memberService.registerMember(
-			Member.builder().name(NAME_SELLER).memberType(MemberType.KAKAO)
-				.email(NAME_SELLER).role(Role.USER).build()
-		);
-		Member buyer = memberService.registerMember(
-			Member.builder().name(NAME_BUYER).memberType(MemberType.KAKAO)
-				.email(NAME_BUYER).role(Role.USER).build()
-		);
-		Product product = productRepository.save(Product.builder().title(TITLE).thumbnailImage(IMAGE_URL).category(category).member(seller).build());
-
-		Offer offer = offerRepository.save(Offer.builder().product(product).member(buyer).build());
+		Category category = categoryFactory.createCategory("목욕 용품");
+		Member seller = memberFactory.createMember();
+		Product product = productFactory.createProduct(seller, category, 20000);
+		Member buyer = memberFactory.createMember();
+		Offer offer = offerFactory.createOffer(product, buyer);
 		DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder().selectedTime(LocalDateTime.now()).build();
 
-		// when
 		offerService.acceptOffer(offer.getId(), decideOfferRequest);
 		Deal deal = dealRepository.findByProduct(product);
+
 		String codeSellerGotFromBuyer = dealService.getCode(deal.getId(), buyer.getId()); // 판매자가 구매자의 QR코드를 읽어 코드를 얻었다
 
-		// then
+		// when & then
 		assertNotNull(dealService.finishDeal(deal.getId(), seller.getId(), codeSellerGotFromBuyer));
 	}
+
+	@Test
+	@Transactional
+	void 거래_예정_거래_조회() throws Exception {
+		// given
+		LocalDateTime now = LocalDateTime.now();
+		Category category = categoryFactory.createCategory("목욕 용품");
+		Member seller = memberFactory.createMember();
+
+		List<LocalDateTime> dealTimeList = List.of(new LocalDateTime[]{
+			now.plusHours(1),
+			now.plusHours(1).plusMinutes(1).withSecond(0).withNano(0),
+			now.plusHours(1).withSecond(59)
+		});
+
+		for (LocalDateTime dealTime : dealTimeList) {
+			Product product = productFactory.createProduct(seller, category, 20000);
+			Member buyer = memberFactory.createMember();
+			Offer offer = offerFactory.createOffer(product, buyer);
+			DecideOfferRequest decideOfferRequest = DecideOfferRequest.builder()
+					.selectedTime(dealTime)
+					.build();
+			offerService.acceptOffer(offer.getId(), decideOfferRequest);
+//			log.info("dealTime은 : {}", dealTime);
+		}
+
+		Pageable pageable = PageRequest.of(0,10);
+
+		// when
+		List<Deal> scheduledDealList = dealService.findScheduledDeal(now, pageable);
+
+		// then
+		assertEquals(2, scheduledDealList.size());
+	}
+
 }
