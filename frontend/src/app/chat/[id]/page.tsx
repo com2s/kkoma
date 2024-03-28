8d0c83f14bf36ea57db45f2c85ff540817e69cc2
zoowb <qlenfrl101@naver.com>
2024-03-28 18:28:58 +0900

    💄 style: 채팅 화면 만들기
    #S10P22A308-370

--- frontend/src/app/chat/[id]/page.tsx
+++ frontend/src/app/chat/[id]/page.tsx
@@ -4,11 +4,15 @@ import SockJS from "sockjs-client";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import Image from "next/image";
 import TopBar3 from "@/components/common/top-bar3";
 import styles from "./chat.module.scss";
 import { getChatListAPI } from "@/services/chat";
 import { Chat } from "@/types/chat";
 import LocalStorage from "@/utils/localStorage";
+
+import SendIcon from "@mui/icons-material/Send";
+import { ProductCard } from "@/components/common/product-card";
 
 let ws: CompatClient;
 
@@ -64,17 +68,47 @@ export default function Chatting() {
   return (
     <div className={styles.chat}>
       <TopBar3 />
-      {chatList && chatList.length > 0 ? (
-        chatList.map((item, k) => (
-          <div key={k}>
-            <span>{item.memberProfile.nickname}</span>:<span>{item.content}</span>
-          </div>
-        ))
-      ) : (
-        <></>
-      )}
-      <input type="text" onChange={handleChatInput} value={msg} />
-      <button onClick={sendChat}>전송</button>
+      <div className={styles.product}>
+        <ProductCard
+          product={{
+            id: 0,
+            thumbnail_image: "https://picsum.photos/250/250",
+            title: "제품이름",
+            price: 1200,
+          }}
+        />
+      </div>
+      <section className={styles.chatBody}>
+        {chatList && chatList.length > 0 ? (
+          chatList.map((item, k) => (
+            <div
+              key={k}
+              className={item.memberProfile.id === 2 ? styles.chatRight : styles.chatLeft}
+            >
+              <Image
+                className={styles.profile}
+                src={item.memberProfile.profileImage}
+                alt="profile"
+                width={30}
+                height={30}
+              />
+              <div className={styles.chatBox}>
+                <span className={styles.nickname}>{item.memberProfile.nickname}</span>
+                <span className={styles.content}>{item.content}</span>
+                <span className={styles.time}>{item.sentTime}</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <></>
+        )}
+      </section>
+      <div className={styles.inputLine}>
+        <input type="text" onChange={handleChatInput} value={msg} placeholder="채팅 입력" />
+        <button onClick={sendChat}>
+          <SendIcon className="c-text1" />
+        </button>
+      </div>
     </div>
   );
 }
