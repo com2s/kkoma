ba3772d05fe97dd9fd6b285c8aab8905625f6b9e
김이현 <rihyun1126@gmail.com>
2024-03-29 22:25:51 +0900

    :memo: chore: 찜 관련 API Swagger summary 추가

--- backend/src/main/java/com/ssafy/kkoma/api/product/controller/WishController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/controller/WishController.java
@@ -9,6 +9,9 @@ import com.ssafy.kkoma.domain.product.entity.WishList;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfo;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfoDto;
 import com.ssafy.kkoma.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Pageable;
@@ -22,6 +25,11 @@ public class WishController {
 
     private final ProductService productService;
 
+    @Tag(name = "Wish")
+    @Operation(
+            summary = "거래 글 찜하기",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @PostMapping("/{productId}/wish")
     public ResponseEntity<ApiUtils.ApiResult<ProductWishResponse>> wishProduct(
         @MemberInfo MemberInfoDto memberInfoDto,
@@ -32,6 +40,11 @@ public class WishController {
         return ResponseEntity.ok(ApiUtils.success(response));
     }
 
+    @Tag(name = "Wish")
+    @Operation(
+            summary = "거래 글 찜 취소하기",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @PutMapping("/{productId}/unwish")
     public ResponseEntity<?> unwishProduct(
         @MemberInfo MemberInfoDto memberInfoDto,
@@ -42,6 +55,11 @@ public class WishController {
         return ResponseEntity.ok(ApiUtils.success(response));
     }
 
+    @Tag(name = "Wish")
+    @Operation(
+            summary = "내가 찜한 모든 거래 글 조회",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @GetMapping("/wishes")
     public ResponseEntity<ApiUtils.ApiResult<BasePageResponse<WishList, ProductSummary>>> getMyWishProducts(
             @MemberInfo MemberInfoDto memberInfoDto,
