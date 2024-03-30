82991e014ac34519d50682a47376685339426fd9
김이현 <rihyun1126@gmail.com>
2024-03-30 22:58:23 +0900

    :memo: chore: 최근 열람한 거래 글 조회 Swagger summary 추가

--- backend/src/main/java/com/ssafy/kkoma/api/product/controller/ViewHistoryController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/product/controller/ViewHistoryController.java
@@ -7,6 +7,9 @@ import com.ssafy.kkoma.domain.product.entity.ViewHistory;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfo;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfoDto;
 import com.ssafy.kkoma.global.util.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +24,16 @@ public class ViewHistoryController {
 
     private final ViewHistoryService viewHistoryService;
 
+    @Tag(name = "ViewHistory")
+    @Operation(
+            summary = "최근 열람한 거래 글 조회",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
     @GetMapping("/viewHistories")
-    public ResponseEntity<ApiUtils.ApiResult<BasePageResponse<ViewHistory, ProductSummary>>> getMyViewHistories(@MemberInfo MemberInfoDto memberInfoDto, Pageable pageable) {
+    public ResponseEntity<ApiUtils.ApiResult<BasePageResponse<ViewHistory, ProductSummary>>> getMyViewHistories(
+            @MemberInfo MemberInfoDto memberInfoDto,
+            Pageable pageable
+    ) {
         BasePageResponse<ViewHistory, ProductSummary> myViewHistoriesResponse = viewHistoryService.getMyViewHistories(memberInfoDto.getMemberId(), pageable);
         return ResponseEntity.ok(ApiUtils.success(myViewHistoriesResponse));
     }
