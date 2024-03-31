7f47cd76d41ef5a77eb51ed4c59558d6de44500b
이수현 <olafminion0116@naver.com>
2024-03-31 12:34:57 +0900

    ✨ feat: 포인트 히스토리 전체 조회 API 추가

--- backend/src/main/java/com/ssafy/kkoma/api/point/controller/PointController.java
+++ backend/src/main/java/com/ssafy/kkoma/api/point/controller/PointController.java
@@ -1,8 +1,12 @@
 package com.ssafy.kkoma.api.point.controller;
 
+import com.ssafy.kkoma.api.common.dto.BasePageResponse;
+import com.ssafy.kkoma.api.point.dto.PointHistorySummary;
 import com.ssafy.kkoma.api.point.dto.PointSummaryResponse;
+import com.ssafy.kkoma.api.point.service.PointHistoryService;
 import com.ssafy.kkoma.api.point.service.PointService;
 import com.ssafy.kkoma.api.product.service.ProductService;
+import com.ssafy.kkoma.domain.point.entity.PointHistory;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfo;
 import com.ssafy.kkoma.global.resolver.memberinfo.MemberInfoDto;
 import com.ssafy.kkoma.global.util.ApiUtils;
@@ -10,6 +14,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PointController {
 
     private final PointService pointService;
+    private final PointHistoryService pointHistoryService;
 
     @Tag(name = "Point")
     @Operation(summary = "to get a point summary", security = {@SecurityRequirement(name = "bearer-key")})
@@ -34,7 +40,6 @@ public class PointController {
         return ResponseEntity.ok(ApiUtils.success(pointSummary));
     }
 
-
     @Tag(name = "Point")
     @Operation(summary = "to get a point summary", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("/products/{productId}")
@@ -44,5 +49,21 @@ public class PointController {
     ) {
         pointService.comparePointsToPrice(memberInfoDto.getMemberId(), productId);
         return ResponseEntity.ok().build();
+    }
+
+    @Tag(name = "Point")
+    @Operation(
+            summary = "포인트 히스토리 전체 조회",
+            description = "[[노션](https://www.notion.so/todays-jiwoo/e2afab87fc3842728c08f00104e01428?pvs=4)] ",
+            security = { @SecurityRequirement(name = "bearer-key") }
+    )
+    @GetMapping("/history")
+    public ResponseEntity<?> getPointHistory(
+        @MemberInfo MemberInfoDto memberInfoDto,
+        Pageable pageable
+    ){
+        BasePageResponse<PointHistory, PointHistorySummary> pointHistorySummaryListResponse =
+                pointHistoryService.getPointHistory(memberInfoDto.getMemberId(), pageable);
+        return ResponseEntity.ok(ApiUtils.success(pointHistorySummaryListResponse));
     }
 }
--- backend/src/main/java/com/ssafy/kkoma/api/point/service/PointHistoryService.java
+++ backend/src/main/java/com/ssafy/kkoma/api/point/service/PointHistoryService.java
@@ -1,16 +1,21 @@
 package com.ssafy.kkoma.api.point.service;
 
+import com.ssafy.kkoma.api.common.dto.BasePageResponse;
+import com.ssafy.kkoma.api.member.service.MemberService;
+import com.ssafy.kkoma.api.point.dto.PointHistorySummary;
 import com.ssafy.kkoma.domain.member.entity.Member;
 import com.ssafy.kkoma.domain.point.constant.PointChangeType;
 import com.ssafy.kkoma.domain.point.entity.Point;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
 import com.ssafy.kkoma.domain.point.entity.PointHistory;
 import com.ssafy.kkoma.domain.point.repository.PointHistoryRepository;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -18,12 +23,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PointHistoryService {
 
+	private final MemberService memberService;
 	private final PointHistoryRepository pointHistoryRepository;
 
 	public void changePoint(Member member, PointChangeType pointChangeType, int amount) {
 		Point point = member.getPoint();
 
-		if (pointChangeType == PointChangeType.CHARGE ||pointChangeType == PointChangeType.PROFIT) {
+		if (pointChangeType == PointChangeType.CHARGE || pointChangeType == PointChangeType.PROFIT) {
 			point.addBalance(amount);
 		} else {
 			point.subBalance(amount);
@@ -36,6 +42,17 @@ public class PointHistoryService {
 				.balanceAfterChange(point.getBalance())
 				.build();
 		pointHistoryRepository.save(pointHistory);
+	}
+
+	public BasePageResponse<PointHistory, PointHistorySummary> getPointHistory(Long memberId, Pageable pageable) {
+		Member member = memberService.findMemberByMemberId(memberId);
+		Page<PointHistory> page = pointHistoryRepository.findByPoint(member.getPoint(), pageable);
+
+		List<PointHistorySummary> content = page.getContent().stream()
+			.map(PointHistorySummary::fromEntity)
+			.toList();
+
+		return new BasePageResponse<>(content, page);
 	}
 
 	public void createPointHistory(PointHistory pointHistory) {
