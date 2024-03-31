947f458d05cd627d80535824838fddefefd4b36d
이수현 <olafminion0116@naver.com>
2024-04-01 00:59:41 +0900

    👔 feat: 눈물의 batch 작업 보류(주석 처리), 요청 수락 및 거래 생성 시 스케쥴링 통해 거래 알림 예약

--- backend/src/main/java/com/ssafy/kkoma/scheduler/ReadDealTableScheduler.java
+++ backend/src/main/java/com/ssafy/kkoma/scheduler/ReadDealTableScheduler.java
@@ -19,33 +19,33 @@ import java.time.LocalDateTime;
 @Component
 public class ReadDealTableScheduler {
 
-    @Autowired
-    private Job readDealTableJob;
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    private LocalDateTime lastRun = LocalDateTime.now();
-    private LocalDateTime curRun;
-
-    @Scheduled(cron = "0 */2 * * * *") // once per 2 minutes (at the top of the minute - the 0th second).
-    public void readDealTableJobRun() throws
-            JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException,
-            JobParametersInvalidException, JobRestartException
-    {
-        curRun = LocalDateTime.now();
-        log.info("[1/lastRun] {} ", lastRun);
-        log.info("[1/curRun] {}", curRun);
-
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLocalDateTime("lastRun", lastRun)
-                .addLocalDateTime("curRun", curRun)
-                .toJobParameters();
-
-        lastRun = curRun;
-        log.info("[2/lastRun] {} ", lastRun);
-        log.info("[2/curRun] {}", curRun);
-
-        jobLauncher.run(readDealTableJob, jobParameters);
-    }
+//    @Autowired
+//    private Job readDealTableJob;
+//
+//    @Autowired
+//    private JobLauncher jobLauncher;
+//
+//    private LocalDateTime lastRun = LocalDateTime.now();
+//    private LocalDateTime curRun;
+//
+//    @Scheduled(cron = "0 */2 * * * *") // once per 2 minutes (at the top of the minute - the 0th second).
+//    public void readDealTableJobRun() throws
+//            JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException,
+//            JobParametersInvalidException, JobRestartException
+//    {
+//        curRun = LocalDateTime.now();
+//        log.info("[1/lastRun] {} ", lastRun);
+//        log.info("[1/curRun] {}", curRun);
+//
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addLocalDateTime("lastRun", lastRun)
+//                .addLocalDateTime("curRun", curRun)
+//                .toJobParameters();
+//
+//        lastRun = curRun;
+//        log.info("[2/lastRun] {} ", lastRun);
+//        log.info("[2/curRun] {}", curRun);
+//
+//        jobLauncher.run(readDealTableJob, jobParameters);
+//    }
 }
