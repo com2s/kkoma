f54a1c2869a2ba0ff840765bec26b84f635b9ab7
최지우 <qlenfrl101@naver.com>
2024-03-31 13:11:52 +0900

    🩹 fix: name, phone 안받도록 변경

--- frontend/src/app/join/name/page.tsx
+++ frontend/src/app/join/name/page.tsx
@@ -1,28 +1,28 @@
-"use client";
+// "use client";
 
-import { ButtonContainer, NormalBtn } from "@/components/common/buttons";
-import TextField from "@mui/material/TextField";
-import { useRecoilState } from "recoil";
-import { userNameState } from "@/store/join";
+// import { ButtonContainer, NormalBtn } from "@/components/common/buttons";
+// import TextField from "@mui/material/TextField";
+// import { useRecoilState } from "recoil";
+// import { userNameState } from "@/store/join";
 
-export default function AddName() {
-  const [name, setName] = useRecoilState(userNameState);
+// export default function AddName() {
+//   const [name, setName] = useRecoilState(userNameState);
 
-  return (
-    <>
-      <h2>{`이름을 알려주세요`}</h2>
-      <TextField
-        id="standard-basic"
-        label="이름"
-        variant="standard"
-        sx={{ width: "100%", fontWeight: "bold" }}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <ButtonContainer>
-        <NormalBtn next={"/join/phone"} disabled={name == null || name == ""}>
-          다음
-        </NormalBtn>
-      </ButtonContainer>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2>{`이름을 알려주세요`}</h2>
+//       <TextField
+//         id="standard-basic"
+//         label="이름"
+//         variant="standard"
+//         sx={{ width: "100%", fontWeight: "bold" }}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <ButtonContainer>
+//         <NormalBtn next={"/join/phone"} disabled={name == null || name == ""}>
+//           다음
+//         </NormalBtn>
+//       </ButtonContainer>
+//     </>
+//   );
+// }
--- frontend/src/app/join/nickname/page.tsx
+++ frontend/src/app/join/nickname/page.tsx
@@ -2,11 +2,37 @@
 
 import { ButtonContainer, NormalBtn } from "@/components/common/buttons";
 import TextField from "@mui/material/TextField";
-import { useRecoilState } from "recoil";
-import { userNicknameState } from "@/store/join";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { userProfileState, userNicknameState } from "@/store/join";
+import { updateMemberAPI } from "@/services/member";
+import { uploadImagesAPI } from "@/services/upload";
+import { useRouter } from "next/navigation";
 
 export default function AddNickname() {
+  const profile = useRecoilValue(userProfileState);
   const [nickname, setNickname] = useRecoilState(userNicknameState);
+
+  const router = useRouter();
+
+  const joinMember = async () => {
+    if (nickname !== null) {
+      let profileImage;
+      if (profile) {
+        profileImage = await uploadImagesAPI(profile);
+        profileImage = await profileImage[0];
+      } else {
+        profileImage =
+          "https://kkoma.s3.ap-northeast-2.amazonaws.com/images/e3a028fb-3355-4888-afd6-21a15a1f4c95.png";
+      }
+      const obj = await updateMemberAPI({
+        profileImage: profileImage,
+        nickname: nickname,
+      });
+
+      router.replace("/join/complete");
+    }
+    //TODO: 에러 페이지로 이동
+  };
 
   return (
     <>
@@ -19,8 +45,11 @@ export default function AddNickname() {
         onChange={(e) => setNickname(e.target.value)}
       />
       <ButtonContainer>
-        <NormalBtn next={"/join/name"} disabled={nickname == null || nickname == ""}>
-          다음
+        <NormalBtn
+          next={joinMember}
+          disabled={nickname == null || nickname == ""}
+        >
+          완료
         </NormalBtn>
       </ButtonContainer>
     </>
