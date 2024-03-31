4c42a1128c81657850e791562ced7533d78cdc85
최지우 <qlenfrl101@naver.com>
2024-03-31 21:02:30 +0900

    ✨ feat: 지도 중심 좌표, 주소 구하기

--- frontend/src/components/common/map.tsx
+++ frontend/src/components/common/map.tsx
@@ -10,27 +10,51 @@ declare global {
 
 // TODO: 사용자 위치 받아오기, 마커 찍기, 마커 주소 받아오기
 export default function Home() {
-  // .env.local 파일에 API 키를 저장하고 불러온다.
-  // const MAP_API_KEY 에 저장하지 않고 바로 쓸 수도 있다.
+  let map: any, geocoder: any;
   const MAP_API_KEY = process.env.NEXT_PUBLIC_KAKAO_JS_KEY;
+
+  const displayCenterInfo = (result: any, status: any) => {
+    if (status === window.kakao.maps.services.Status.OK) {
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].region_type === "H") {
+          console.log("중심?", result[i].address_name);
+          break;
+        }
+      }
+    }
+  };
+
+  const searchAddrFromCoords = (coords: any, callback: any) => {
+    geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+  };
 
   useEffect(() => {
     const kakaoMapScript = document.createElement("script");
     kakaoMapScript.async = false;
-    // 카카오 API 키를 쓸 때 먼저 우리의 사이트 도메인을 등록해야한다.
-    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${MAP_API_KEY}&autoload=false`;
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${MAP_API_KEY}&autoload=false&libraries=services`;
     console.log(kakaoMapScript.src);
     document.head.appendChild(kakaoMapScript);
 
     const onLoadKakaoAPI = () => {
       window.kakao.maps.load(() => {
-        var container = document.getElementById("map");
-        var options = {
+        let container = document.getElementById("map");
+        let options = {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
 
-        var map = new window.kakao.maps.Map(container, options);
+        map = new window.kakao.maps.Map(container, options);
+        geocoder = new window.kakao.maps.services.Geocoder();
+
+        window.navigator.geolocation.getCurrentPosition((p) => {
+          let lat = p.coords.latitude;
+          let lng = p.coords.longitude;
+          map.setCenter(new window.kakao.maps.LatLng(lat, lng));
+        });
+
+        window.kakao.maps.event.addListener(map, "idle", () =>
+          searchAddrFromCoords(map.getCenter(), displayCenterInfo)
+        );
       });
     };
 
