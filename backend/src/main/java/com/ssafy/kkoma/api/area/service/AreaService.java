c543de9866f300fd55c6dac07eaa40765a8253c0
soyeon <jjellyduck@gmail.com>
2024-04-01 15:56:32 +0900

    ✨ feat: edit location range in search product
    #S10P22A308-458

--- backend/src/main/java/com/ssafy/kkoma/api/area/service/AreaService.java
+++ backend/src/main/java/com/ssafy/kkoma/api/area/service/AreaService.java
@@ -10,6 +10,7 @@ import com.ssafy.kkoma.domain.area.entity.Area;
 import com.ssafy.kkoma.domain.area.repository.AreaRepository;
 import com.ssafy.kkoma.global.error.ErrorCode;
 import com.ssafy.kkoma.global.error.exception.BusinessException;
+import com.ssafy.kkoma.global.util.AreaCodeUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,29 +22,17 @@ public class AreaService {
 	private final AreaRepository areaRepository;
 
 	public List<SubArea> getArea(Long code) {
-		int level = code == null ? 0 : codeLevel(code);
+		int level = code == null ? 0 : AreaCodeUtils.getLevel(code);
 		if(level == 4) {
 			throw new BusinessException(ErrorCode.SUB_AREA_NOT_EXISTS);
 		}
 
-		long digit = levelDigit(level);
-		long nextDigit = levelDigit(level + 1);
+		long digit = AreaCodeUtils.getDigitByLevel(level);
+		long nextDigit = AreaCodeUtils.getDigitByLevel(level + 1);
 
 		return areaRepository.getArea(level, code, digit, nextDigit).stream()
 				.map(sub -> SubArea.fromEntity(sub, level))
 				.collect(Collectors.toList());
-	}
-
-
-	private long levelDigit(int level) {
-		return (long)Math.pow(10, 11 - (level * 3));
-	}
-
-	private int codeLevel(long code) {
-		for (int i = 1; i < 4; i++) {
-			if(code % levelDigit(i) == 0) return i;
-		}
-		return 4;
 	}
 
 }
