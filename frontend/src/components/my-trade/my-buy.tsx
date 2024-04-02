b2600401ea1717dd89e89536e24f9ade5791a8f6
최지우 <qlenfrl101@naver.com>
2024-04-03 02:25:49 +0900

    💄 fix: update my-buy UI

--- frontend/src/components/my-trade/my-buy.tsx
+++ frontend/src/components/my-trade/my-buy.tsx
@@ -3,11 +3,7 @@ import { getMyProducts } from "@/components/my-trade/my-trade-ftn";
 import { Deal } from "@/types/status";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
@@ -15,8 +11,11 @@ import styles from "@/components/my-trade/sell-buy.module.scss";
 import { NoContents } from "../common/no-contents";
 import { SmallBtn } from "../common/buttons";
 import Image from "next/image";
+import { ProductCard } from "../common/product-card";
+import { useRouter } from "next/navigation";
 
 export default function MyBuy() {
+  const router = useRouter();
   const [deals, setDeals] = useState<Deal[]>([]);
   const [success, setSuccess] = useState(true);
   const [selectedChip, setSelectedChip] = useState<string>("모두");
@@ -57,7 +56,7 @@ export default function MyBuy() {
     if (window.confirm("거래 요청을 취소하시겠습니까?")) {
       // 여기에 거래 요청 취소 API 호출 코드 추가
       handleMenuClose();
-      console.log("거래 요청 취소");
+      router.refresh();
     }
   };
 
@@ -97,81 +96,8 @@ export default function MyBuy() {
         ))}
       </Stack>
       <div className="flex flex-col gap-5">
-        {filteredDeals.map((deal) => (
-          <Card key={deal.id} variant="outlined" className={styles.card}>
-            <Avatar
-              alt="Product Image"
-              src={deal.thumbnailImage}
-              sx={{ width: 80, height: 80 }}
-              className={styles.avatar}
-              variant="square"
-            />
-            <CardContent sx={{ padding: 1 }} className={styles.cardMiddle}>
-              <Typography variant="h6" component="div">
-                {deal.price.toLocaleString()}원{" "}
-              </Typography>
-              <Typography color="text.secondary">{deal.title}</Typography>
-              <Typography variant="body2">
-                {deal.dealPlace ?? "거래 장소 null"} •{" "}
-                {deal.elapsedMinutes >= 1440 // 1440분 = 24시간
-                  ? `${Math.floor(deal.elapsedMinutes / 1440)}일 전`
-                  : deal.elapsedMinutes >= 60
-                  ? `${Math.floor(deal.elapsedMinutes / 60)}시간 전`
-                  : `${deal.elapsedMinutes}분 전`}
-              </Typography>
-              <Typography variant="caption" className="text-gray-400">
-                조회 {deal.viewCount} • 거래 요청 {deal.offerCount} • 찜{" "}
-                {deal.wishCount}
-              </Typography>
-            </CardContent>
-            <CardContent
-              sx={{
-                padding: 0,
-                margin: 0,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <IconButton
-                aria-label="settings"
-                onClick={(e) => handleMenuClick(e, deal.id)} // 클릭 핸들러에 deal.id 전달
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                anchorEl={anchorEl}
-                open={open && openMenuId === deal.id} // 현재 카드의 메뉴만 열림
-                onClose={handleMenuClose}
-              >
-                <MenuItem onClick={handleMenuCloseAndCancel}>
-                  {/* 거래 id : {openMenuId}  */}
-                  거래 요청 취소
-                </MenuItem>
-                {/* <MenuItem onClick={handleMenuClose}>2번째 버튼 임시</MenuItem> */}
-              </Menu>
-              <Typography
-                variant="body1"
-                textAlign={"center"}
-                sx={{
-                  mt: 2,
-                  fontWeight: "bold",
-                  width: "55px",
-                  color:
-                    // deal.status2 의 값에 따라 색상을 다르게 표시
-                    deal.status === "CANCELLED"
-                      ? "crimson"
-                      : deal.status === "SOLD"
-                      ? "dimgray"
-                      : deal.status === "SENT"
-                      ? "orange"
-                      : "black", // 기본값
-                }}
-              >
-                {deal.status}
-              </Typography>
-            </CardContent>
-          </Card>
+        {filteredDeals.map((deal, k) => (
+          <ProductCard product={deal} next={`/lists/${deal.id}`} key={k} />
         ))}
         {filteredDeals.length === 0 && (
           <NoContents>
