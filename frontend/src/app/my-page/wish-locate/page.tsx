edabdbabde815f7d8099abe5ed7b97d01e2b450f
zoowb <qlenfrl101@naver.com>
2024-04-02 09:21:44 +0900

    ✨ feat: update preferred place api
    #S10P22A308-379

--- frontend/src/app/my-page/wish-locate/page.tsx
+++ frontend/src/app/my-page/wish-locate/page.tsx
@@ -8,19 +8,21 @@ import Map from "@/components/common/map";
 import Title from "@/components/common/title";
 import TopBar3 from "@/components/common/top-bar3";
 import { ButtonContainer, NormalBtn } from "@/components/common/buttons";
+import { updatePreferredPlaceAPI } from "@/services/member";
 
 export default function WishLocate() {
   const [location, setLocation] = useState("");
   const [regionCode, setRegionCode] = useState("");
 
+  const handleUpdate = async () => {
+    await updatePreferredPlaceAPI(regionCode);
+  };
+
   return (
     <div className={styles.locate}>
       <TopBar3 />
       <div className="flex justify-end pb-2 w-full "></div>
-      <Title
-        title="거래하고 싶은 장소를 선택해주세요"
-        subtitle="누구나 찾기 쉬운 공공장소가 좋아요"
-      />
+      <Title title="선호하는 거래 장소를 선택해주세요" subtitle="시군구 단위로 선택할 수 있어요" />
       <Image src={"/images/Pin.png"} alt="pin" width={100} height={100} />
       {location ? (
         <div className="text-caption !text-white bg-gray-500 py-1 px-2 rounded-xl w-fit	text-center">
@@ -29,10 +31,10 @@ export default function WishLocate() {
       ) : (
         <></>
       )}
-      <Map setLocation={setLocation} setRegionCode={setRegionCode} />
+      <Map setRegion={setLocation} setRegionCode={setRegionCode} />
       <div className="w-full">
         <ButtonContainer>
-          <NormalBtn next={"/"}>선택</NormalBtn>
+          <NormalBtn next={handleUpdate}>선택</NormalBtn>
         </ButtonContainer>
       </div>
     </div>
--- frontend/src/services/member.ts
+++ frontend/src/services/member.ts
@@ -22,3 +22,21 @@ export const updateMemberAPI = async (memberInfo: MemberInfo) => {
     //TODO: error 페이지로 이동
   }
 };
+
+export const updatePreferredPlaceAPI = async (regionCode: string) => {
+  try {
+    const res = await APIModule({
+      action: "/members/place",
+      method: "PATCH",
+      data: { preferredPlaceRegionCode: regionCode },
+    });
+    if (res.success) {
+      return res.data;
+    } else {
+      //TODO: error 페이지로 이동
+      throw new Error(res.error.errorMessage);
+    }
+  } catch (e: any) {
+    //TODO: error 페이지로 이동
+  }
+};
