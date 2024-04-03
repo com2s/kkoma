bbf96a29e887c1c9d0bb72e9aecddac27f8ee8e7
zoowb <qlenfrl101@naver.com>
2024-04-04 03:59:51 +0900

    💄 fix: update main page
    #S10P22A308-454

--- frontend/src/components/home/hourlyProductList.tsx
+++ frontend/src/components/home/hourlyProductList.tsx
@@ -4,13 +4,17 @@ import { ProductSm } from "@/types/product";
 
 interface Products {
   title: string;
+  subtitle?: string;
   products: Array<ProductSm>;
 }
 
-export default function HourlyProductList({ title, products }: Products) {
+export default function HourlyProductList({ title, subtitle, products }: Products) {
   return (
     <div className={styles.container}>
-      <h4>{title}</h4>
+      <div>
+        <h4>{title}</h4>
+        {subtitle && <div className="text-caption c-text3 mt-1">{subtitle}</div>}
+      </div>
       <div className={styles.list}>
         {products.map((item, k) => {
           return <ProductCard key={k} product={item} />;
