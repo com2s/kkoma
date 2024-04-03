ac79cdf355f9a91792049848717a38fcc4138629
이수현 <olafminion0116@naver.com>
2024-04-03 16:50:30 +0900

    🐛 fix: redis 환경변수 설정 및 test 상수 변경

--- backend/src/test/java/com/ssafy/kkoma/factory/LocationFactory.java
+++ backend/src/test/java/com/ssafy/kkoma/factory/LocationFactory.java
@@ -14,7 +14,7 @@ public class LocationFactory {
 	LocationRepository locationRepository;
 
 	public Location createLocation() {
-		final Long REGIONCODE = 1111011300L;
+		final Long REGIONCODE = 1111010100L;
 
 		return locationRepository.save(Location.builder()
 			.regionCode(REGIONCODE)
